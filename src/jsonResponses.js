1a0c6d15a50b335eeb804e36d553531811c588ad
katie-b9030 <klb9030@rit.edu>
2025-02-23 21:46:42 -0500

    fixed runtime errors

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,7 +1,7 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const movies = fs.readFileSync('../resources/disney_movies.json');
-const favoriteData = fs.readFileSync('./favorites.json');
+const movies = fs.readFileSync("./resources/disney_movies.json");
+const favoriteData = fs.readFileSync("./resources/favorites.json");
 
 const moviesJSON = JSON.parse(movies);
 let selectedMovies = {};
@@ -12,35 +12,35 @@ const selectMovies = (url) => {
   const keys = Object.keys(moviesJSON);
   keys.forEach((key) => {
     const movie = moviesJSON[key];
-    if (url.contains('?')) {
-      const parameter = url.split('?')[1];
-      let info = parameter.split('=')[1];
+    if (url.contains("?")) {
+      const parameter = url.split("?")[1];
+      let info = parameter.split("=")[1];
 
-      if (parameter.contains('title')) {
+      if (parameter.contains("title")) {
         const { title } = movie;
-        info.replace(/_/g, ' ');
+        info.replace(/_/g, " ");
 
         if (title.toLowerCase() === info) {
           selectedMovies[movie.title] = movie;
         }
-      } else if (parameter.contains('year')) {
-        let releases = movie['Release date'];
+      } else if (parameter.contains("year")) {
+        let releases = movie["Release date"];
 
-        if (typeof releases === 'string') {
+        if (typeof releases === "string") {
           releases = [releases];
         }
-        releases.map((release) => release.split(', ')[1]);
+        releases.map((release) => release.split(", ")[1]);
 
         releases.forEach((release) => {
           if (release.year === info) {
             selectedMovies[movie.title] = movie;
           }
         });
-      } else if (parameter.contains('actor')) {
+      } else if (parameter.contains("actor")) {
         let actors = movie.Starring;
-        info.replace(/_/g, ' ');
+        info.replace(/_/g, " ");
 
-        if (typeof actors === 'string') {
+        if (typeof actors === "string") {
           actors = [actors];
         }
 
@@ -49,11 +49,11 @@ const selectMovies = (url) => {
             selectedMovies[movie.title] = movies;
           }
         });
-      } else if (parameter.contains('director')) {
-        let directors = movie['Directed by'];
-        info.replace(/_/g, ' ');
+      } else if (parameter.contains("director")) {
+        let directors = movie["Directed by"];
+        info.replace(/_/g, " ");
 
-        if (typeof directors === 'string') {
+        if (typeof directors === "string") {
           directors = [directors];
         }
 
@@ -62,45 +62,45 @@ const selectMovies = (url) => {
             selectedMovies[movie.title] = movies;
           }
         });
-      } else if (parameter.contains('grossing')) {
-        const grossing = movie['Box office (float)'];
+      } else if (parameter.contains("grossing")) {
+        const grossing = movie["Box office (float)"];
 
-        if (info.contains('>')) {
-          info = parseInt(info.split('>')[1], 10);
+        if (info.contains(">")) {
+          info = parseInt(info.split(">")[1], 10);
 
           if (grossing > info) {
             selectedMovies[movie.title] = movies;
           }
-        } else if (info.contains('<')) {
-          info = parseInt(info.split('<')[1], 10);
+        } else if (info.contains("<")) {
+          info = parseInt(info.split("<")[1], 10);
 
           if (grossing < info) {
             selectedMovies[movie.title] = movies;
           }
         }
-      } else if (parameter.contains('duration')) {
-        const duration = movie['Running time (int)'];
+      } else if (parameter.contains("duration")) {
+        const duration = movie["Running time (int)"];
 
-        if (info.contains('>')) {
-          info = parseInt(info.split('>')[1], 10);
+        if (info.contains(">")) {
+          info = parseInt(info.split(">")[1], 10);
 
           if (duration > info) {
             selectedMovies[movie.title] = movies;
           }
-        } else if (info.contains('<')) {
-          info = parseInt(info.split('<')[1], 10);
+        } else if (info.contains("<")) {
+          info = parseInt(info.split("<")[1], 10);
 
           if (duration < info) {
             selectedMovies[movie.title] = movies;
           }
         }
-      } else if (parameter.contains('imdb')) {
+      } else if (parameter.contains("imdb")) {
         const imdb = movie.imdb_rating;
 
         if (imdb === info) {
           selectedMovies[movie.title] = movies;
         }
-      } else if (parameter.contains('tomatoes')) {
+      } else if (parameter.contains("tomatoes")) {
         const tomatoes = movie.rotten_tomatoes;
 
         if (tomatoes === info) {
@@ -123,10 +123,10 @@ const getAllMovies = (request, response) => {
   const responseMessage = JSON.stringify(responseData);
 
   response.writeHead(200, {
-    'Content-Type': 'application/json',
-    'Content-Length': Buffer.byteLength(responseMessage, 'utf-8'),
+    "Content-Type": "application/json",
+    "Content-Length": Buffer.byteLength(responseMessage, "utf-8"),
   });
-  if (request.method !== 'HEAD') {
+  if (request.method !== "HEAD") {
     response.write(responseMessage);
   }
   response.end();
@@ -144,10 +144,10 @@ const getMovieTitles = (request, response) => {
   const responseMessage = JSON.stringify(responseData);
 
   response.writeHead(200, {
-    'Content-Type': 'application/json',
-    'Content-Length': Buffer.byteLength(responseMessage, 'utf-8'),
+    "Content-Type": "application/json",
+    "Content-Length": Buffer.byteLength(responseMessage, "utf-8"),
   });
-  if (request.method !== 'HEAD') {
+  if (request.method !== "HEAD") {
     response.write(responseMessage);
   }
   response.end();
@@ -163,10 +163,10 @@ const getMovieByRating = (request, response) => {
   const responseMessage = JSON.stringify(responseData);
 
   response.writeHead(200, {
-    'Content-Type': 'application/json',
-    'Content-Length': Buffer.byteLength(responseMessage, 'utf-8'),
+    "Content-Type": "application/json",
+    "Content-Length": Buffer.byteLength(responseMessage, "utf-8"),
   });
-  if (request.method !== 'HEAD') {
+  if (request.method !== "HEAD") {
     response.write(responseMessage);
   }
   response.end();
@@ -182,10 +182,10 @@ const getMovieByDetails = (request, response) => {
   const responseMessage = JSON.stringify(responseData);
 
   response.writeHead(200, {
-    'Content-Type': 'application/json',
-    'Content-Length': Buffer.byteLength(responseMessage, 'utf-8'),
+    "Content-Type": "application/json",
+    "Content-Length": Buffer.byteLength(responseMessage, "utf-8"),
   });
-  if (request.method !== 'HEAD') {
+  if (request.method !== "HEAD") {
     response.write(responseMessage);
   }
   response.end();
@@ -201,50 +201,50 @@ const getFavoriteMovies = (request, response) => {
   const responseMessage = JSON.stringify(responseData);
 
   response.writeHead(200, {
-    'Content-Type': 'application/json',
-    'Content-Length': Buffer.byteLength(responseMessage, 'utf-8'),
+    "Content-Type": "application/json",
+    "Content-Length": Buffer.byteLength(responseMessage, "utf-8"),
   });
-  if (request.method !== 'HEAD') {
+  if (request.method !== "HEAD") {
     response.write(responseMessage);
   }
   response.end();
 };
 
 const addFavoriteMovie = (request, response) => {
-  let bodyData = '';
+  let bodyData = "";
   let selectedFavorite;
 
   let responseData;
   let status;
 
-  request.on('data', (chunk) => {
+  request.on("data", (chunk) => {
     bodyData += chunk;
   });
 
-  request.on('end', () => {
+  request.on("end", () => {
     const keys = Object.keys(moviesJSON);
     keys.forEach((key) => {
       const movie = moviesJSON[key];
       if (bodyData.toLowerCase() === movie.title) {
         selectedFavorite = movie;
         responseData = {
-          message: 'Created Successfully',
+          message: "Created Successfully",
         };
         status = 201;
       } else {
         responseData = {
-          id: 'noMatchingTitle',
-          message: 'Provided title does not match an existing movie.',
+          id: "noMatchingTitle",
+          message: "Provided title does not match an existing movie.",
         };
         status = 400;
       }
     });
 
-    fs.writeFileSync('favorites.json', selectedFavorite);
+    fs.writeFileSync("favorites.json", selectedFavorite);
 
     const responseMessage = JSON.stringify(responseData);
     response.writeHead(status, {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     });
     if (status !== 204) {
       response.write(responseMessage);
@@ -254,27 +254,27 @@ const addFavoriteMovie = (request, response) => {
 };
 
 const addMovieRating = (request, response) => {
-  let bodyData = '';
+  let bodyData = "";
   let status;
   let responseData;
 
-  request.on('data', (chunk) => {
+  request.on("data", (chunk) => {
     bodyData += chunk;
   });
 
-  request.on('end', () => {
+  request.on("end", () => {
     const keys = Object.keys(moviesJSON);
     const data = JSON.parse(bodyData);
 
     keys.forEach((key) => {
       const movie = moviesJSON[key];
       if (data.title.toLowerCase() === movie.title) {
-        movie['Personal Rating'] = data.rating;
+        movie["Personal Rating"] = data.rating;
         status = 204;
       } else {
         responseData = {
-          id: 'noMatchingTitle',
-          message: 'Provided title does not match an existing movie.',
+          id: "noMatchingTitle",
+          message: "Provided title does not match an existing movie.",
         };
         status = 400;
       }
@@ -282,7 +282,7 @@ const addMovieRating = (request, response) => {
 
     const responseMessage = JSON.stringify(responseData);
     response.writeHead(status, {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     });
     if (status !== 204) {
       response.write(responseMessage);
@@ -293,16 +293,16 @@ const addMovieRating = (request, response) => {
 
 const getNotFound = (request, response) => {
   const responseData = {
-    id: 'notFound',
-    message: 'The page you are looking for was not found',
+    id: "notFound",
+    message: "The page you are looking for was not found",
   };
 
   const responseMessage = JSON.stringify(responseData);
   response.writeHead(404, {
-    'Content-Type': 'application/json',
-    'Content-Length': Buffer.byteLength(responseMessage, 'utf-8'),
+    "Content-Type": "application/json",
+    "Content-Length": Buffer.byteLength(responseMessage, "utf-8"),
   });
-  if (request.method !== 'HEAD') {
+  if (request.method !== "HEAD") {
     response.write(responseMessage);
   }
   response.end();
